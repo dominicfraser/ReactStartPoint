2e308b1f9ef2b6d94901a60f1c6a69d8231114d7
Dominic Fraser <mrdjfraser@gmail.com>
2017-11-12 14:55:49 +0000

    updated to use babel-preset-env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,7 +15,7 @@ var config = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
         }
       }
     ]
